3889bae72cb65415cecaad32bc61f40910bc110d
AGYTgit <bubonb12@gmail.com>
2024-12-19 22:47:40 +0100

    final

--- main.py
+++ main.py
@@ -4,52 +4,92 @@ from time import sleep
 from math import cos, sin, pi
 from neopixel import NeoPixel
 
+buttonPin = Pin(7, Pin.IN, Pin.PULL_UP)
+buttonPin = Pin(16, Pin.IN, Pin.PULL_DOWN)
+
 motors = [
-    [PWM(Pin(10, Pin.OUT), freq=1000, duty= 0),
-     PWM(Pin(11, Pin.OUT), freq=1000, duty= 0)],
-    [ PWM(Pin(12, Pin.OUT), freq=1000, duty= 1000),
-      PWM(Pin(13, Pin.OUT), freq=1000, duty= 0)]]
+    [PWM(Pin(10, Pin.OUT), freq=1000, duty= 0), PWM(Pin(11, Pin.OUT), freq=1000, duty= 0)],
+    [PWM(Pin(12, Pin.OUT), freq=1000, duty= 0), PWM(Pin(13, Pin.OUT), freq=1000, duty= 0)]
+]
     
 np = NeoPixel(Pin(14), 1)
-np.fill((0,255,0))
+np.fill((0,5,0))
 np.write()
 
 
-
-def defeated():
-    np.fill((255,0,0))
+def defeated(pin):
+    np.fill((5,0,0))
     np.write()
     for motor in motors:
         for direction in motor:
             direction.duty(0)
             
-    sleep(5)
+    sleep(10)
     np.fill((0,0,0))
     np.write()
+    while True:
+        pass
+    
+buttonPin.irq(trigger=Pin.IRQ_RISING, handler=defeated)
+buttonPin.irq(trigger=Pin.IRQ_RISING, handler=defeated)
     
 
-def move(angle, distance, max_dist):
-    d = cos(angle)
-    max_speed = 1
-    speed = mapping(distance,0,max_dist, 0, max_speed)
-    print(d)
-    if d > 0 :
-        if d > 0.5:
-            print("L ",d*speed, " R ", max_speed - d*speed)
-        else:
-            print("L ", max_speed - d*speed, " R ",d*speed)
-    elif d < 0:
-        if d > -0.5:
-            print("L -",d*speed, " R -", max_speed - d*speed)
-        else:
-            print("L -", max_speed - d*speed, " R -",d*speed)
-    else:
-        print("L", max_speed, " R ",max_speed)
+from math import pi
+
+def move(a, s, sMax):
+    angle = a;
+    speedMax = mapping(s, 0, sMax, 0, 1000)
+    sL = speedMax
+    sR = speedMax
     
-def mapping(x,in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    if angle >= 0:
+        if angle <= pi/4:
+            sL = mapping(angle, 0, pi/4, speedMax, 0)
+        elif angle <= pi/2:
+            sL = mapping(angle, pi/4, pi/2, 0, -speedMax)
+    elif angle < 0:
+        if angle >= -pi/4:
+            sR = mapping(-angle, 0, pi/4, speedMax, 0)
+        elif angle >= -pi/2:
+            sR = mapping(-angle, pi/4, pi/2, 0, -speedMax)
+    if angle > pi/2:
+        sR = -sR
+        if angle <= pi*(3/4):
+            sL = mapping(angle, pi/2, pi*(3/4), speedMax, 0)
+        elif angle <= pi:
+            sL = mapping(angle, pi*(3/4), pi, 0, -speedMax)
+    elif angle < -pi/2:
+        sL = -sL
+        sR = -sR
+        if angle >= -pi*(3/4):
+            sR = mapping(-angle, pi/2, pi*(3/4), speedMax, 0)
+        elif angle >= -pi:
+            sR = mapping(-angle, pi*(3/4), pi, 0, -speedMax)
+    
+    if sL >= 0:
+        motors[0][0].duty(int(sL))
+    else:
+        motors[0][1].duty(int(-sL))
+        
+    if sR >= 0:
+        motors[1][0].duty(int(sR))
+    else:
+        motors[1][1].duty(int(-sR))
+        
+    print(angle, sL, sR)
+
+def mapping(value, in_min, in_max, out_min, out_max):
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
        
 
-#if __name__ == "__main__":
-#    wifi = WIFI()
-#    wifi.init()
+def start():
+    wifi = WIFI()
+    wifi.init()
+    while True:
+        try:
+            angle, distance, max_dist = wifi.check_connection();
+            move(angle, distance, max_dist)
+        except:
+            pass
+
+
--- server.py
+++ server.py
@@ -8,6 +8,7 @@ class WIFI:
         self.ssid = 'ESP32_AP'        # Network name
         self.password = '12345678'    # Password (minimum 8 characters)
         self.html_content = self.import_html()
+        self.sock = None
         
     def create_ap(self):
         ap = network.WLAN(network.AP_IF)  # Create Access Point interface
@@ -21,67 +22,50 @@ class WIFI:
         print('Access Point created successfully!')
         print('Network details:', ap.ifconfig())
 
-    def import_html(self):
+    def import_html(self): 
         with open('./index.html', 'r', encoding='utf-8') as file:
             return file.read()
         
     def parse_post_data(self, request):
-        # Decode the byte data to a string (use utf-8 instead of gzip)
         request_str = request.decode('utf-8')
-        print(f"Decoded request: {request_str}")
 
-        # Extract POST data (after the headers, the body is separated by \r\n\r\n)
         post_data = request_str.split("\r\n\r\n")[-1]
-        print(f"POST Data: {post_data}")
 
-        # Split the post data by '&' to separate the angle and distance values
         data = post_data.split(" ")
 
-        # Get the angle and distance as floats
-        print(data)
-        angle = float(data[0])  # Get the angle value
-        distance = float(data[1])  # Get the distance value
-        max_dist = 1 
-
-        print(f"Parsed values - Angle: {angle}, Distance: {distance}")
+        angle = float(data[0])
+        distance = float(data[1])
+        max_dist = float(data[2])
 
         return angle, distance, max_dist
 
     
     def http_server(self):
         # Create socket for HTTP server
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind(('', 80))
-        sock.listen(5)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(('', 80))
+        self.sock.listen(5)
         print('HTTP server running...')
         
-        while True:
-            client, addr = sock.accept()
-            print(f'Client connected: {addr}')
             
-            request = client.recv(1024)
-            
-            """
-            if b'GET /favicon' or b'GET /apple-touch-icon' in request:
-                client.send('HTTP/1.1 204 No Content\n')  # No content, no favicon
-                client.send('Connection: close\n\n')
-                client.close()
-                continue
-            """
-            
-            print('')
-            print(f'Request: {request}')
-            print('')
-            
-            if 'POST' in request:
-                distance, angle, max_dist = self.parse_post_data(request)
-                print(f"Received message: {distance}, {angle}, {max_dist}")
-            
-            client.send('HTTP/1.1 200 OK\n')
-            client.send('Content-Type: text/html\n')
-            client.send('Connection: close\n\n')
-            client.sendall(self.html_content.encode('utf-8'))
-            client.close()
+    def check_connection(self):
+        distance, angle, max_dist = -1,0,0
+        client, addr = self.sock.accept()
+        print(f'Client connected: {addr}')
+        
+        request = client.recv(1024)
+        
+        if 'POST' in request:
+            angle, distance, max_dist = self.parse_post_data(request)
+        
+        client.send('HTTP/1.1 200 OK\n')
+        client.send('Content-Type: text/html\n')
+        client.send('Connection: close\n\n')
+        client.sendall(self.html_content.encode('utf-8'))
+        client.close()
+        
+        if distance is not -1:
+            return angle, distance, max_dist
 
     def init(self):
         self.create_ap()
